91850bea48fc6ff08758b44a3797e15eaa7205e5
auguNa <augustin.nae@gmail.com>
2024-06-03 10:40:41 +0200

    S1_04 REady to be checked

--- src/S1_04/N3Ejercicio2/PersonTest.java
+++ src/S1_04/N3Ejercicio2/PersonTest.java
@@ -18,7 +18,6 @@ class PersonTest {
         Person person1 = new Person("Alice");
         Person person2 = new Person("Alice");
         //Person person2 = person1;
-        // This assertion will pass because person1 and person2 reference different objects
         Assertions.assertThat(person1).isNotSameAs(person2);
     }
 
--- src/S1_04/N3Ejercicio4/ArrayListTestTest.java
+++ src/S1_04/N3Ejercicio4/ArrayListTestTest.java
@@ -21,26 +21,21 @@ class ArrayListTestTest {
         list.add(dbl);
         list.add(bool);
 
-        // Check the order of the objects as they were inserted
-
         assertEquals(str, list.get(0));
         assertEquals(num, list.get(1));
         assertEquals(dbl, list.get(2));
         assertEquals(bool, list.get(3));
 
-        // Verify that the list contains the objects in any order
         assertTrue(list.contains(str));
         assertTrue(list.contains(num));
         assertTrue(list.contains(dbl));
         assertTrue(list.contains(bool));
 
-        // Verify that each object has been added only once
         assertEquals(1, list.stream().filter(e -> e.equals(str)).count());
         assertEquals(1, list.stream().filter(e -> e.equals(num)).count());
         assertEquals(1, list.stream().filter(e -> e.equals(dbl)).count());
         assertEquals(1, list.stream().filter(e -> e.equals(bool)).count());
 
-        // Verify that an object that was not added is not in the list
         Character ch = 'A';
         assertFalse(list.contains(ch));
     }
